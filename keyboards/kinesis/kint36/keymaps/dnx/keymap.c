6427048ed980118afcb7e80749da3602ecd34223
DNX <auraltension@riseup.net>
2021-10-13 14:16:19 +1100

    mv ctrl-a

--- keyboards/kinesis/kint36/keymaps/dnx/keymap.c
+++ keyboards/kinesis/kint36/keymaps/dnx/keymap.c
@@ -69,13 +69,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
            // Left Hand
            KC_ESC,    KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,KC_F7  ,KC_F8,
            _______,   KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,
-           KC_GRV,    KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,
+           LCTL(KC_A),    KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,
            LGUI_T(KC_EQL),  KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,
            KC_LSFT,   LCTL_T(KC_Z)   ,KC_X   ,KC_C   ,KC_V   ,LALT_T(KC_B)   ,
                      XXXXXXX ,KC_INS ,KC_LEFT,KC_RGHT,
            // Left Thumb
 			                  KC_HOME,KC_END ,
-                                                  LCTL(KC_A),
+                                                  _______,
 //                           TG(1),KC_BSPC ,KC_ESC ,
                            LT(1, KC_SPC),KC_BSPC ,KC_ESC ,
            // Right Hand
@@ -111,7 +111,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
            _______, KC_P7,   KC_P8,   KC_P9,   KC_PMNS,  _______,
            _______, KC_P4,   KC_P5,   KC_P6,   KC_PPLS,  _______,
            _______, KC_P1,   KC_P2,   KC_P3,   _______,  _______,
-                    KC_PD0,  _______, KC_PDOT, _______,
+                    KC_P0,   _______, KC_PDOT, _______,
            // Right Thumb
            _______, _______,
            _______,
